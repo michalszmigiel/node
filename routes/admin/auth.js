2db213c91777993148b26672c1a6929ccabafafc
Michał R. Szmigiel <michal.r.szmigiel@gmail.com>
2020-01-24 10:49:38 +0100

    routes

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+const { handleErrors } = require("./middlewares");
 
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
@@ -21,18 +21,14 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
 
     const user = await usersRepo.create({ email, password });
     req.session.userId = user.id;
-    res.send(`acc created`);
+
+    res.redirect("/admin/products");
   }
 );
 
@@ -48,20 +44,14 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    console.log(errors);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
 
     req.session.userId = user.id;
 
-    res.send(`You are signed in`);
+    res.redirect("/admin/products");
   }
 );
 
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,31 +1,82 @@
 const express = require("express");
-const { validationResult } = require("express-validator");
 const multer = require("multer");
 
+const { handleErrors, requireAuth } = require("./middlewares");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
+const productsEditTemplate = require("../../views/admin/products/edit");
+const productsIndexTemplate = require("../../views/admin/products/index");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get("/admin/products", (req, res) => {});
+router.get("/admin/products", requireAuth, async (req, res) => {
+  const products = await productsRepo.getAll();
+  res.send(productsIndexTemplate({ products }));
+});
 
-router.get("/admin/products/new", (req, res) => {
+router.get("/admin/products/new", requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
 router.post(
   "/admin/products/new",
-  [requireTitle, requirePrice],
+  requireAuth,
   upload.single("image"),
-  (req, res) => {
-    const errors = validationResult(req);
+  [requireTitle, requirePrice],
+  handleErrors(productsNewTemplate),
+  async (req, res) => {
+    if (!req.session.userId) {
+      return res.redirect("/signin");
+    }
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
 
-    console.log(req.file);
-
-    res.send("submitted");
+    return res.status(201).redirect("/admin/products/");
   }
 );
+
+router.get("/admin/products/:id/edit", requireAuth, async (req, res) => {
+  const product = await productsRepo.getOne(req.params.id);
+
+  if (!product) {
+    return res.send("Product not found");
+  }
+
+  res.send(productsEditTemplate({ product }));
+});
+
+router.post(
+  "/admin/products/:id/edit",
+  requireAuth,
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  handleErrors(productsEditTemplate, async req => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
+  async (req, res) => {
+    const changes = req.body;
+
+    if (req.file) {
+      changes.image = req.file.buffer.toString("base64");
+    }
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      res.send("Could not find item");
+    }
+
+    res.redirect("/admin/products");
+  }
+);
+
+router.post("/admin/products/:id/delete", requireAuth, async (req, res) => {
+  await productsRepo.delete(req.params.id);
+
+  res.redirect("/admin/products");
+});
 
 module.exports = router;
